19f7f757ffb31da39e879ef493ae0ae3380c2807
Jonathan Yu <jy178@duke.edu>
2019-01-27 02:44:40 -0500

    Added comments for Simulation class, its methods, and its fields

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,8 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Superclass for all simulations
+ * <p>
+ * This class creates a simulation grid and provides functionality to update the grid.
+ */
 public abstract class Simulation {
 
+    // grid is in (x, y) coordinate form, so the outer array represents the columns and the inner array represents the
+    // element of each row in a particular column
     private Cell[][] grid;
     private int gridSideSize;
 
@@ -12,10 +19,23 @@ public abstract class Simulation {
         populateGrid();
     }
 
+    /**
+     * Fills the grid with Cell objects
+     */
     protected abstract void populateGrid();
 
-    public abstract Cell[][] step();
+    /**
+     * Calculates the next state for each Cell object in the grid, then updates the grid
+     */
+    public abstract void step();
 
+    /**
+     * Gets the neighbors of a particular cell in the grid
+     * @param x x-coordinate of the cell
+     * @param y y-coordinate of the cell
+     * @return list of ordered pairs, representing the neighbors, in the order {N, NE, E, SE, S, SW, W, NW} where
+     * neighbor cells that are not in the grid (invalid) are null
+     */
     protected List<int[]> getNeighbors(int x, int y) {
         List<int[]> neighbors = new ArrayList<>();
 
@@ -47,6 +67,12 @@ public abstract class Simulation {
         return neighbors;
     }
 
+    /**
+     * Determines if a particular cell is in the grid or not
+     * @param x x-coordinate of the cell
+     * @param y y-coordinate of the cell
+     * @return true if the cell is in the grid (valid), false otherwise (invalid)
+     */
     protected boolean isCellValid(int x, int y) {
         if (x < 0 || x >= gridSideSize || y < 0 || y >= gridSideSize) {
             return false;
@@ -55,6 +81,10 @@ public abstract class Simulation {
         return true;
     }
 
+    /**
+     * Returns the grid for other classes to access
+     * @return the simulation grid
+     */
     public Cell[][] getGrid() {
         return grid;
     }

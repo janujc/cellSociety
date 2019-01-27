8fe04b75b4e8a05f1b8479580d353911d4f403ac
Jonathan Yu <jy178@duke.edu>
2019-01-27 04:13:43 -0500

    Refactored getNeighbors() into getAllNeighbors(), getCardinalNeighbors(), getCornerNeighbors(), and validateNeighbors()

--- src/Simulation.java
+++ src/Simulation.java
@@ -35,60 +35,89 @@ public abstract class Simulation {
     public abstract void step();
 
     /**
-     * Gets the neighbors of a particular cell in the grid
+     * Gets the all neighbors (cardinal and corner) of a particular cell in the grid
      * @param x x-coordinate of the cell
      * @param y y-coordinate of the cell
      * @return map of neighbors where the key is the neighbor's coordinates (int array where 0th index is the neighbor's
      * x-coordinate and the 1st index is the neighbor's y-coordinate) and the value is the state of the neighbor
      */
-    protected Map<int[], Integer> getNeighbors(int x, int y) {
+    protected Map<int[], Integer> getAllNeighbors(int x, int y) {
         Map<int[], Integer> neighbors = new HashMap<>();
 
-        // list of the coordinates of the cell's neighbors, represented by an array, where the 0th index is the
-        // neighbor's x-coordinate and the 1st index is the neighbor's y-coordinate
-        List<int[]> neighborCoords = new ArrayList<>();
-        // N
-        neighborCoords.add(new int[]{x, y - 1});
-        // NE
-        neighborCoords.add(new int[]{x + 1, y - 1});
-        // E
-        neighborCoords.add(new int[]{x + 1, y});
-        // SE
-        neighborCoords.add(new int[]{x + 1, y + 1});
-        // S
-        neighborCoords.add(new int[]{x, y + 1});
-        // SW
-        neighborCoords.add(new int[]{x - 1, y + 1});
-        // W
-        neighborCoords.add(new int[]{x - 1, y});
-        //NW
-        neighborCoords.add(new int[]{x -1, y - 1});
-
-        // mark invalid neighbors (not in grid) null
-        for (int i = 0; i < neighborCoords.size(); i ++) {
-            int[] neighbor = neighborCoords.get(i);
-            int neighborX = neighbor[0];
-            int neighborY = neighbor[1];
-            if (isCellValid(neighborX, neighborY)) {
-                neighbors.put(neighbor, grid[neighborX][neighborY]);
-            }
-        }
+        // as the coordinates of the cardinal and corner neighbors are distinct, no worry about overwriting entries
+        neighbors.putAll(getCardinalNeighbors(x, y));
+        neighbors.putAll(getCornerNeighbors(x, y));
 
         return neighbors;
     }
 
     /**
-     * Determines if a particular cell is in the grid or not
+     * Gets the cardinal direction neighbors of a particular cell in the grid
      * @param x x-coordinate of the cell
      * @param y y-coordinate of the cell
-     * @return true if the cell is in the grid (valid), false otherwise (invalid)
+     * @return map of neighbors where the key is the neighbor's coordinates (int array where 0th index is the neighbor's
+     * x-coordinate and the 1st index is the neighbor's y-coordinate) and the value is the state of the neighbor
      */
-    protected boolean isCellValid(int x, int y) {
-        if (x < 0 || x >= gridSideSize || y < 0 || y >= gridSideSize) {
-            return false;
+    protected Map<int[], Integer> getCardinalNeighbors(int x, int y) {
+        // list of the coordinates of the cell's neighbors, represented by an array, where the 0th index is the
+        // neighbor's x-coordinate and the 1st index is the neighbor's y-coordinate
+        List<int[]> neighborCoords = new ArrayList<>();
+
+        // N
+        neighborCoords.add(new int[]{x, y - 1});
+        // E
+        neighborCoords.add(new int[]{x + 1, y});
+        // S
+        neighborCoords.add(new int[]{x, y + 1});
+        // W
+        neighborCoords.add(new int[]{x - 1, y});
+        return validateNeighbors(neighborCoords);
+    }
+
+    /**
+     * Gets the corner neighbors of a particular cell in the grid
+     * @param x x-coordinate of the cell
+     * @param y y-coordinate of the cell
+     * @return map of neighbors where the key is the neighbor's coordinates (int array where 0th index is the neighbor's
+     * x-coordinate and the 1st index is the neighbor's y-coordinate) and the value is the state of the neighbor
+     */
+    protected Map<int[], Integer>  getCornerNeighbors(int x, int y) {
+        // list of the coordinates of the cell's neighbors, represented by an array, where the 0th index is the
+        // neighbor's x-coordinate and the 1st index is the neighbor's y-coordinate
+        List<int[]> neighborCoords = new ArrayList<>();
+
+        // NE
+        neighborCoords.add(new int[]{x + 1, y - 1});
+        // SE
+        neighborCoords.add(new int[]{x + 1, y + 1});
+        // SW
+        neighborCoords.add(new int[]{x - 1, y + 1});
+        //NW
+        neighborCoords.add(new int[]{x -1, y - 1});
+
+        return validateNeighbors(neighborCoords);
+    }
+
+    /**
+     * Takes a list possible neighbors and checks that they are in the grid (valid)
+     * @param neighborCoords list of the coordinates of the cell's possible neighbors, represented by an array, where the
+     *                       0th index is the neighbor's x-coordinate and the 1st index is the neighbor's y-coordinate
+     * @return map of valid neighbors where the key is the neighbor's coordinates (int array where 0th index is the
+     * neighbor's x-coordinate and the 1st index is the neighbor's y-coordinate) and the value is the state of the neighbor
+     */
+    private Map<int[], Integer> validateNeighbors(List<int[]> neighborCoords) {
+        Map<int[], Integer> neighbors = new HashMap<>();
+
+        for (int i = 0; i < neighborCoords.size(); i ++) {
+            int[] neighbor = neighborCoords.get(i);
+            int neighborX = neighbor[0];
+            int neighborY = neighbor[1];
+            if (!(neighborX < 0 || neighborX > gridSideSize || neighborY < 0 || neighborY > gridSideSize)) {
+                neighbors.put(neighbor, grid[neighborX][neighborY]);
+            }
         }
 
-        return true;
+        return neighbors;
     }
 
     /**

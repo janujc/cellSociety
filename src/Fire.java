f89b6fcbbc283bdc648684396c7f496ab4f5e72c
Jonathan Yu <jy178@duke.edu>
2019-01-27 02:52:31 -0500

    Added Fire class

---
+++ src/Fire.java
@@ -0,0 +1,9 @@
+public class Fire extends Simulation {
+
+    // the initial probabilities of each state for use by populateGrid()
+    final double FREQ_BURNING = .1;
+    final double FREQ_TREE = .60;
+    final double FREQ_EMPTY = .1 - FREQ_BURNING - FREQ_TREE
+
+    
+}

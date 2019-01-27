1ec55166082bdb213bae809068e7cc0a4907258e
Jonathan Yu <jy178@duke.edu>
2019-01-27 02:12:29 -0500

    Added getNeighbors() to Simulation class

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Simulation {
 
     private Cell[][] grid;
@@ -10,6 +13,29 @@ public abstract class Simulation {
     protected abstract void populateGrid();
 
     public abstract Cell[][] step();
+
+    protected List<int[]> getNeighbors(int x, int y) {
+        List<int[]> neighbors = new ArrayList<>();
+
+        // N
+        neighbors.add(new int[]{x, y - 1});
+        // NE
+        neighbors.add(new int[]{x + 1, y - 1});
+        // E
+        neighbors.add(new int[]{x + 1, y});
+        // SE
+        neighbors.add(new int[]{x + 1, y + 1});
+        // S
+        neighbors.add(new int[]{x, y + 1});
+        // SW
+        neighbors.add(new int[]{x - 1, y + 1});
+        // W
+        neighbors.add(new int[]{x - 1, y});
+        //NW
+        neighbors.add(new int[]{x -1, y - 1});
+
+        return neighbors;
+    }
 
     public Cell[][] getGrid() {
         return grid;

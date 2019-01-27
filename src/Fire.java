4ff8e5aad2379133c941bf5c16e37455aa1cb99b
Jonathan Yu <jy178@duke.edu>
2019-01-27 03:49:09 -0500

    Added Fire constructor and step()

--- src/Fire.java
+++ src/Fire.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Fire extends Simulation {
@@ -8,9 +10,19 @@ public class Fire extends Simulation {
     final int BURNING = 2;
 
     // the initial probabilities of each state for use by populateGrid()
-    final double FREQ_EMPTY = .3;
-    final double FREQ_TREE = .6;
-    final double FREQ_BURNING = 1 - FREQ_EMPTY - FREQ_TREE;
+    final double FREQ_EMPTY;
+    final double FREQ_TREE;
+    final double FREQ_BURNING;
+    // probability that a tree next to a burning tree catches on fire
+    final double PROB_CATCH;
+
+    public Fire(int sideSize, double freqEmpty, double freqTree, double freqBurning, double probCatch) {
+        super(sideSize);
+        FREQ_EMPTY = freqEmpty;
+        FREQ_TREE = freqTree;
+        FREQ_BURNING = freqBurning;
+        PROB_CATCH = probCatch;
+    }
 
     protected void populateGrid() {
         Random rand = new Random();
@@ -28,5 +40,38 @@ public class Fire extends Simulation {
                 }
             }
         }
+    }
+
+    public void step() {
+        // create another grid to hold the updated states, which saves us from writing another nested for loop for
+        // updating the states
+        int[][] updatedGrid = new int[gridSideSize][gridSideSize];
+
+        Random rand = new Random();
+        for (int i = 0; i < gridSideSize; i++) {
+            for (int j = 0; j < gridSideSize; j++) {
+                Map<int[], Integer> neighbors = getNeighbors(i, j);
+                // if a tree is next to a burning tree, it will catch fire with a probability of PROB_CATCH
+                if (grid[i][j] == TREE && neighbors.containsValue(BURNING)) {
+                    int randNum = rand.nextInt(100);
+                    if (randNum < PROB_CATCH * 100) {
+                        updatedGrid[i][j] = BURNING;
+                    }
+                    else {
+                        updatedGrid[i][j] = grid[i][j];
+                    }
+                }
+                // if a tree is burning, it will burn down (become empty cell)
+                else if (grid[i][j] == BURNING) {
+                    updatedGrid[i][j] = EMPTY;
+                }
+                // otherwise, the cell remains the same
+                else {
+                    updatedGrid[i][j] = grid[i][j];
+                }
+            }
+        }
+
+        grid = updatedGrid;
     }
 }

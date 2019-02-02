858695ccd9e2da52983249d2e23233180f6d8c3c
Anshu Dwibhashi <ad353@duke.edu>
2019-02-02 13:18:51 -0500

    Added forgotten ParserTools file

---
+++ src/utils/ParserTools.java
@@ -0,0 +1,24 @@
+package utils;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+public class ParserTools {
+    public static Element getRootNode(File file) {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+            doc.getDocumentElement().normalize();
+            return doc.getDocumentElement();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}

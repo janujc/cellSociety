dec4dc769d5b4eb4140993eb6efb0cfc139a7a45
Januario Carreiro <jjc70@duke.edu>
2019-02-02 16:20:20 -0500

    Added some comments to Segregation and Cell. Possible bugs outlined/

--- src/simulation/Segregation.java
+++ src/simulation/Segregation.java
@@ -79,9 +79,22 @@ public class Segregation extends Simulation {
         }
     }
 
+    /**
+     * Returns an int representing the agent of opposite color. If the input is an empty cell, returns an empty cell.
+     *
+     * e.g.
+     * oppositeAgent(1) returns 2.
+     * 
+     * e.g.
+     * oppositeAgent(0) returns 0.
+     *
+     * @param currState the state of the Cell object.
+     * @return an int corresponding to a different agent.
+     */
     private int oppositeAgent(int currState) {
         if (currState == BLUE_AGENT) return RED_AGENT;
-        return BLUE_AGENT;
+        if (currState == RED_AGENT) return BLUE_AGENT;
+        return EMPTY;
     }
 
     private void determineAgentBehavior(Cell cell, int numSimNeighbors, int numDifNeighbors) {
@@ -98,8 +111,14 @@ public class Segregation extends Simulation {
         satisfiedAgents.add(cell);
     }
 
-    // Remove a cell from dissatisfiedAgents, remove a cell from emptyCell, make
-    // emptyCell.setNextState(state of dissatisfied agent), add cell of dissatisfied agent to emptyCell.
+    /**
+     * Shuffles emptyCell, then for each dissatisfied agent, sets their next state to empty; for each satisfied agent,
+     * sets their next state to the same state.
+     *
+     * TODO: SHOULD I ADD DISSATISFIED AGENTS TO emptyCell?
+     *
+     * TODO: POSSIBLE BUG OCCURS IF THERE ARE NOT ENOUGH Cell OBJECTS FOR EACH Cell OBJECT IN dissatisfiedAgents. ABOVE.
+     */
     private void moveAgents() {
         Collections.shuffle(emptyCell);
         for (Cell agent : dissatisfiedAgents) {

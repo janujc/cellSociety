a1859199b2a5b8ed498fa7b22e3966274bedeb53
Anshu Dwibhashi <ad353@duke.edu>
2019-02-03 18:02:22 -0500

    Added previous state control docs.

--- src/uitools/PrevStateControl.java
+++ src/uitools/PrevStateControl.java
@@ -3,6 +3,10 @@ package uitools;
 import javafx.scene.image.Image;
 import visualization.SimulationScreen;
 
+/**
+ * Author: Anshu Dwibhashi
+ * Control to move to previous state of grid.
+ */
 public class PrevStateControl extends Control {
     private SimulationScreen context;
 

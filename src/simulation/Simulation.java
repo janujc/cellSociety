a07485ef9b664f6baad530c8838caa4780867057
Anshu Dwibhashi <ad353@duke.edu>
2019-02-09 15:51:04 -0500

    Added overloaded simulation constructors.

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -52,7 +52,7 @@ public abstract class Simulation {
     private String currentFileName, displayName;
 
     /**
-     * Creates and populates the simulation grid
+     * Creates and populates the simulation grid randomly based on population frequencies.
      *
      * @param sideSize        the length of one side of the grid
      * @param states          the possible states of the cells in the simulation grid
@@ -65,6 +65,28 @@ public abstract class Simulation {
         colors = stateColors;
         rand = new Random();
         populateGrid(states, populationFreqs);
+    }
+
+    /**
+     * Creates and populates the simulation grid randomly based on number of cells to occupy per state.
+     * @param sideSize      the length of one side of the grid
+     * @param states        the possible states of the cells in the simulation grid
+     * @param numToOccupy   the number of cells each state must occupy
+     * @param stateColors   the cell colors of each state in the simulation
+     */
+    protected  Simulation(int sideSize, Integer[] states, Integer[] numToOccupy, Color[] stateColors) {
+        // TODO
+    }
+
+    /**
+     * Creates and populates the simulation grid randomly based on number of cells to occupy per state.
+     * @param sideSize      the length of one side of the grid
+     * @param states        the possible states of the cells in the simulation grid
+     * @param cells         a 2D array with all cells and their associated state
+     * @param stateColors   the cell colors of each state in the simulation
+     */
+    protected Simulation(int sideSize, Integer[] states, Integer[][] cells, Color[] stateColors) {
+        // TODO
     }
 
     /**
